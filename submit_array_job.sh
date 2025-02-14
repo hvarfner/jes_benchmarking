2f9c05e3f2dd0683d084aa377a4cc47bc2f6659a
hvarfner <carl.hvarfner@live.se>
2025-02-14 13:41:09 +0100

    modified script again

--- submit_array_job.sh
+++ submit_array_job.sh
@@ -1,25 +1,23 @@
-# Slurm script to run 20 seeds for all acquisition functions as an array job
-# Save this script as `run_benchmark.slurm`
-
 #!/bin/bash
-#SBATCH --job-name=botorch_benchmark
-#SBATCH --output=results/slurm_logs/benchmark_%A_%a.out
-#SBATCH --error=results/slurm_logs/benchmark_%A_%a.err
-#SBATCH --array=1-20
+#SBATCH --job-name=${1}${2}_${6}
+#SBATCH --output=slurm_logs/benchmark_%A_%a.err
+#SBATCH --error=slurm_logs/benchmark_%A_%a.err
+#SBATCH --array=1-${5}
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=12:00:00
-#SBATCH -A=naiss2024-22-1657
+#SBATCH -A naiss2024-22-1657
 
 # Load the required modules (adjust based on your cluster setup)
 
 # Define benchmark parameters
-BENCHMARK="Ackley"
-DIMENSION=8
-ITERATIONS=100
-NOISE=0.01
-ACQ="jes"
+BENCHMARK=$1
+DIMENSION=$2
+ITERATIONS=$3
+NOISE=$4
+# SEEDS=$5
+ACQ=$6
 
 python main.py \
     --acq ${ACQ} \
