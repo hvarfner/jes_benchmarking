7ce94e13177b4b1384483d717c9cb913b0f74ffc
hvarfner <carl.hvarfner@live.se>
2025-02-17 14:35:17 +0100

    syntax

--- batch_jobs.sh
+++ batch_jobs.sh
@@ -5,12 +5,12 @@ RESULTS_DIR=$2
 
 for acq in logei jes pes
 do
-    sbatch submit array_job.sh Michalewicz 5 100 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Michalewicz 10 150 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Ackley 8 100 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Ackley 16 150 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Hartmann 4 50 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Hartmann 6 50 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Levy 4 50 $NOISE $acq $RESULTS_DIR
-    sbatch submit array_job.sh Levy 8 100 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Michalewicz 5 100 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Michalewicz 10 150 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Ackley 8 100 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Ackley 16 150 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Hartmann 4 50 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Hartmann 6 50 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Levy 4 50 $NOISE $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Levy 8 100 $NOISE $acq $RESULTS_DIR
 done
--- noise_jobs.sh
+++ noise_jobs.sh
@@ -4,8 +4,9 @@ NOISE=$1
 RESULTS_DIR=$2
 
 for acq in logei jes pes
-    sbatch submit array_job.sh Ackley 6 50 2.5 $acq $RESULTS_DIR
-    sbatch submit array_job.sh Hartmann 4 50 0.5 $acq $RESULTS_DIR
-    sbatch submit array_job.sh Hartmann 6 50 0.5 $acq $RESULTS_DIR
-    sbatch submit array_job.sh Levy 4 50 2.5 $acq $RESULTS_DIR
-end
+do
+    sbatch submit_array_job.sh Ackley 6 50 2.5 $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Hartmann 4 50 0.5 $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Hartmann 6 50 0.5 $acq $RESULTS_DIR
+    sbatch submit_array_job.sh Levy 4 50 2.5 $acq $RESULTS_DIR
+done
